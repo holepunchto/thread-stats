03919dfbe77fc3abd23511e50a5727c5e736cd9d
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-07-05 11:49:49 +0200

    Skip tests on Windows and Linux

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 const test = require('brittle')
+const { isMac } = require('which-runtime')
 const threadStats = require('.')
 
-test('basic', (t) => {
+test('basic', { skip: !isMac }, (t) => {
   t.comment(threadStats())
 })
