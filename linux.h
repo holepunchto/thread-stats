f546fe343e5e5e8d10549761fd552cf38484e198
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-09-04 15:23:38 +0200

    Use a single buffer

--- linux.h
+++ linux.h
@@ -10,71 +10,62 @@ thread_stats__linux (thread_stats_t *stats, size_t *len) {
   int self = getpid();
   clock_t clock_ticks = sysconf(_SC_CLK_TCK);
 
-  float uptime;
+  int fd = open("/proc/uptime", O_RDONLY);
 
-  int uptime_fd;
-  uptime_fd = open("/proc/uptime", O_RDONLY);
-
-  if (uptime_fd == -1) {
+  if (fd == -1) {
     return -1;
   }
 
-  char uptime_buf[4096 + 1];
-  int uptime_buf_read;
+  char buf[4096 + 1 /* NULL */];
+  size_t buf_len;
 
-  uptime_buf_read = read(uptime_fd, uptime_buf, 4096);
+  buf_len = read(fd, buf, 4096);
 
-  err = close(uptime_fd);
+  err = close(fd);
   assert(err == 0);
 
-  if (uptime_buf_read == -1) {
+  if (buf_len == -1) {
     return -1;
   }
 
-  sscanf(uptime_buf, "%f", &uptime);
+  float uptime;
 
-  DIR *self_task_dir;
+  sscanf(buf, "%f", &uptime);
 
-  char self_task_dir_path[4096 + 1];
-  struct dirent *self_task_dir_entry;
+  sprintf(buf, "/proc/%d/task", self);
 
-  sprintf(self_task_dir_path, "/proc/%d/task", self);
+  DIR *dir = opendir(buf);
 
-  self_task_dir = opendir(self_task_dir_path);
-
-  if (self_task_dir == NULL) {
+  if (dir == NULL) {
     return -1;
   }
+
+  struct dirent *entry;
 
   int threads_len = 0;
 
-  while (threads_len < *len && (self_task_dir_entry = readdir(self_task_dir))) {
-    // skip "." and ".." folders
-    if (strstr(self_task_dir_entry->d_name, ".")) {
+  while (threads_len < *len && (entry = readdir(dir))) {
+    // Skip "." and ".." folders
+    if (strstr(entry->d_name, ".")) {
       continue;
     }
 
-    int pid = atoi(self_task_dir_entry->d_name);
+    int pid = atoi(entry->d_name);
 
-    char stat_path[4096 + 1];
-    sprintf(stat_path, "/proc/%d/task/%d/stat", self, pid);
+    sprintf(buf, "/proc/%d/task/%d/stat", self, pid);
 
-    int stat_fd;
-    stat_fd = open(stat_path, O_RDONLY);
+    int stat_fd = open(buf, O_RDONLY);
 
     if (stat_fd == -1) {
       continue;
     }
 
-    char stat_buf[4096 + 1];
-    int stat_buf_read;
-
-    stat_buf_read = read(stat_fd, stat_buf, 4096);
+    buf_len = read(stat_fd, buf, 4096);
 
     err = close(stat_fd);
     assert(err == 0);
 
-    if (stat_buf_read == -1) {
+    if (buf_len == -1) {
       continue;
     }
 
@@ -82,7 +73,7 @@ thread_stats__linux (thread_stats_t *stats, size_t *len) {
     unsigned long long starttime;
 
     // https://www.man7.org/linux/man-pages//man5/proc_pid_stat.5.html
-    sscanf(stat_buf, "%*d %*s %*c %*d %*d %*d %*d %*d %*u %*u %*u %*u %*u %lu %lu %*d %*d %*d  %*d %*d %llu", &utime, &stime, &starttime);
+    sscanf(buf, "%*d %*s %*c %*d %*d %*d %*d %*d %*u %*u %*u %*u %*u %lu %lu %*d %*d %*d  %*d %*d %llu", &utime, &stime, &starttime);
 
     thread_stats_t *stat = &stats[threads_len];
 
@@ -93,7 +84,7 @@ thread_stats__linux (thread_stats_t *stats, size_t *len) {
     ++threads_len;
   }
 
-  err = closedir(self_task_dir);
+  err = closedir(dir);
   assert(err == 0);
 
   *len = threads_len;
